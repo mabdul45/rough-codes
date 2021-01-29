7d8e64a052b8fea7e3c38dad7a49baecf8451d96
mabdul45 <62806024+mabdul45@users.noreply.github.com>
2021-01-29 17:41:11 +0100

    Add files via upload

---
+++ menu icon toggle/web.js
@@ -0,0 +1,9 @@
+function toggleChangeClass() {
+    var x = document.body;
+  x.classList.toggle("change");
+}
+
+function toggleTheme() {
+  var color = document.body;
+  color.classList.toggle("dark-mode")
+}
